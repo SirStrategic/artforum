81e12a2ec07e42c795d24c458e42c823201339d4
SirStrategic <rocketscienceluca@gmail.com>
2021-11-17 17:27:45 +1100

    alot

--- Forum/forumhome.js
+++ Forum/forumhome.js
@@ -3,36 +3,49 @@ var url = require('url');
 var fs = require('fs');
 var dt = require('./threadloader');
 var events = require('events');
+var formidable = require('formidable');
 
-http.createServer(function (req, res) {
+http.createServer(function (req, res){
     var parsedurl = url.parse(req.url, true);
-   // if (parsedurl.pathname == "/" || parsedurl.pathname == "\\") {
-   //     parsedurl.pathname = "INVALID";
-   // }
+    //if (parsedurl.pathname == "/" || parsedurl.pathname == "\\") {
+    //    parsedurl.pathname = "INVALID";
+    //}
     var urlfilereq = "." + parsedurl.pathname;
-    console.log(parsedurl.pathname);
+    
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+          res.write('File uploaded');
+          console.log("message posted");
+          res.end();
+        });
+
+    }else if(req.url == '/'){
+        res.writeHead(302, {'Location': './forumIndex.html'});
+          res.end();
+    }else{
     fs.readFile(urlfilereq, function (err, data) {
         if (err) {
-            console.log("-------------------------------------------------------------------------");
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.write("404 file not found");
-            return res.end();
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write("404 File Not Found");
+            res.write("<br><img src=\"404image.png\" alt=\"404 image\" style=\"width:300px;height:auto;\">")
+            return res.end("");
         }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data); 
+        res.end();
         });
-    console.log("less fucked");
-           
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!');
-    //res.write(data);
+        
         /*
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write("<form = \"messagebuffer\" method = \"post\">");
-        res.write("<textarea id=\"w3review\" name=\"userpost\" rows=\"4\" cols=\"50\">");
-        res.write("<label for = \"userpost\">Enter your post here</label>");
-        res.write("<input type = \"submit\" value = \"Post Message\">"); //ask if sure to post
-        res.write("</form");
+        res.write("<form = \"messagebuffer\" method = \"post\">\n");
+        res.write("<label for = \"userpost\">Enter your post here</label><br>\n");
+        res.write("<textarea id=\"postbox\" name=\"userpost\" rows=\"4\" cols=\"50\"></textarea><br>\n");
+        res.write("<input type = \"submit\" value = \"Post Message\">\n"); //ask if sure to post
+        res.write("</form>");
         return res.end();
         */
-    return res.end();
+    }
+    
+    //return res.end();
 }).listen(8080, '192.168.0.20');
