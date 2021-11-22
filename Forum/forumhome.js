22751bea99ca6531f22b86e5aa7762d21ff85f06
SirStrategic <rocketscienceluca@gmail.com>
2021-11-22 21:20:40 +1100

    feels good man, when it just works man

--- Forum/forumhome.js
+++ Forum/forumhome.js
@@ -10,7 +10,6 @@ var threadloader = require('./threadloader');
 //var threadviewer = require('./viewer');
 var events = require('events');
 var formidable = require('formidable');
-const app = express();
 
 http.createServer(function (req, res){
     var parsedurl = url.parse(req.url, true);
@@ -32,7 +31,7 @@ http.createServer(function (req, res){
         res.writeHead(302, {'Location': './forumIndex.html'});
           res.end();
     }else if(req.url == '/usernamechecks'){
-        //TEMPORARY TEST
+        
          
 
          let data = '';
@@ -48,7 +47,7 @@ http.createServer(function (req, res){
                 
             try{
                result = threadloader.checkIfUserExists(data);//idk what the return type is lol printout is yellow so prob real bool but whatever
-               //console.log("recieved result" + result);
+               console.log("check requested through /usernamechecks, recieved result" + result);
                //result = (result=="true");
                //console.log(result);
                //console.log(threadloader.checkIfUserExists(data));
@@ -67,7 +66,7 @@ http.createServer(function (req, res){
                 res.end();
             }else{
                 responsejson.bad = result;
-                responsejson.message = data + " is a valid, currently unused username";
+                responsejson.message = "[" + data + "]" + " is a valid, currently unused username";
                 res.write(JSON.stringify(responsejson));
                 res.end();
             }
@@ -83,7 +82,7 @@ http.createServer(function (req, res){
 
 
          
-    }else if(req.url == '/signup'){
+    }else if(req.url == '/signupform'){
          res.writeHead(200, {'Content-Type': 'text/html'});
         //todo: do preliminary tests make sure message recieved
         let data = '';
@@ -91,7 +90,9 @@ http.createServer(function (req, res){
            data += chunk;
          });
          req.on('end', () => {
-            console.log(data);
+            console.log("aaaaaaaaaaaaaaaaaaaaaa"+data);
+            res.write("recieved");
+            res.end();
          });
 
         
@@ -100,15 +101,21 @@ http.createServer(function (req, res){
          //verify username
          //rehash password
          //submit to database
+         //idk if I even need this here, might even thow an error lol
          res.end();
     }else{
-    fs.readFile(urlfilereq, function (err, data) {
-        if (err) {
+        fs.readFile(urlfilereq, function (err, data) {
+        //IMPORTANT QUESTION: can these files be requested in any other way? 
+        permittedpages=["./signup.html", "./forumIndex.html", "./ForumStyle.css", "./Index.html", "./galaryIndex.html", "./threadviewer.html", "./404image.png"]; //my having to include images means that they are requested through this, tha tis good, I can controll more like that
+        //why does it want dot here but not above
+        console.log("A client has requested: " + urlfilereq);
+        if (err || !permittedpages.includes(urlfilereq)) {
             res.writeHead(404, {'Content-Type': 'text/html'});
             res.write("404 File Not Found");
             res.write("<br><img src=\"404image.png\" alt=\"404 image\" style=\"width:300px;height:auto;\">")
             return res.end("");
         }
+        //console.log(data); this comes out as bytes
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data); 
 
